da1e1fc11ea2cfc51c12eb41331a3076f73fe629
Mehradi <mehradi.info@gmail.com>
2023-08-09 21:51:53 -0800

    Adding datas.json

--- src/Circles.tsx
+++ src/Circles.tsx
@@ -4,7 +4,8 @@ interface Iprop {
   width: number;
   height: number;
 }
+const margin = { t: 30, r: 20, b: 50, l: 50 };
 const Circles: FC<Iprop> = ({ year, width, height }) => {
-  return <div>Circles</div>;
+  return <svg viewBox={`0 0 width height`}></svg>;
 };
 export default Circles;
