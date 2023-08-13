0979bc6ac13db1d10467590dcb12bc96e1e0b0df
Mehradi <mehradi.info@gmail.com>
2023-08-13 10:08:19 +0200

    Adding Legend(2)

--- src/Legend.tsx
+++ src/Legend.tsx
@@ -1,9 +1,30 @@
-import React, { FC } from 'react';
-interface Iprop {
-  
-    }
-const Legend: FC<Iprop> = ({}) => {
-  
-  return(<div>Legend</div>);
+import { ScaleOrdinal } from "d3";
+import React, { FC } from "react";
+interface IProp {
+  labels: string[];
+  colorScale: ScaleOrdinal<string, any, never>;
+  setContinent: (continent: string) => void;
+  continent: string;
+}
+const Legend: FC<IProp> = ({ labels, colorScale, setContinent, continent }) => {
+  return (
+    <div className="legend-container">
+      {labels.map((l) => {
+        return (
+          <div
+            style={{ fontWeight: continent === l ? 700 : 400 }}
+            className="legend-row"
+          >
+            <div
+              style={{ backgroundColor: colorScale(l) }}
+              onClick={() => setContinent(l)}
+              className="legend-marker"
+            ></div>
+            <p className="legend-label">{l}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 export default Legend;
