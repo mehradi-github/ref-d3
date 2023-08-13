8d9e966afa56cf4352241c3ca35e19fc39be9ba8
Mehradi <mehradi.info@gmail.com>
2023-08-13 10:13:08 +0200

    Adding Legend in App

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,22 @@ import Circles from "./Circles";
 import data from "./datas.json";
 import { scaleOrdinal, schemeTableau10 } from "d3";
 import Slider from "react-input-slider";
+import Legend from "./Legend";
 
 const width = 960;
 const height = 500;
 function App() {
   const [year, setYear] = useState({ x: 1987 });
+  const [selectedContinent, setSelectedContinent] = useState("all");
 
   const continents = [...new Set(data.map((d) => d.continent))];
   const color = scaleOrdinal().domain(continents).range(schemeTableau10);
+
+  function handleLegendClick(continent: string) {
+    setSelectedContinent((prevState) =>
+      prevState === continent ? "all" : continent
+    );
+  }
   return (
     <div className="App">
       <h1 className="header">D3.js chart</h1>
@@ -26,6 +34,12 @@ function App() {
           onChange={({ x }) => setYear((state) => ({ ...state, x }))}
         ></Slider>
       </div>
+      <Legend
+        setContinent={handleLegendClick}
+        continent={selectedContinent}
+        colorScale={color}
+        labels={continents}
+      />
       <div className="chart">
         <Circles
           year={year.x}
